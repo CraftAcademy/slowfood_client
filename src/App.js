6cbc2db49dbb1c5c003595f2f92f37cd86343765
oliverochman <oliver.ochman@hotmail.com>
2020-07-07 11:00:00 +0200

    user can login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react'
 import Menu from "./components/Menu";
+import Login from "./components/Login";
 
-const App = () => {
-  return (
-    <>
-      <h1>Slowfood</h1>
-      <Menu />
-    </>
-  );
+
+class App extends Component {
+  state = {
+    authenticated: false
+  }
+
+  render() {
+    let login
+    this.state.authenticated ? (
+      login = (
+        <p id="message">You are currently logged in as {JSON.parse(sessionStorage.getItem("credentials")).uid}</p>
+      )
+    ) : (
+      login = (
+        <Login 
+          setAuthenticated={() => this.setState({authenticated: true})}
+        />
+      )
+    )
+
+    return (
+      <>
+        {login}
+        <h1>Slowfood</h1>
+        <Menu />
+      </>
+    )
+  }
 }
 
 export default App;
